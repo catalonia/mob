34ae02ffb64fdb52626b107be1731b2861015da4
dusseltal <jagmohan.khulbe@gmail.com>
2013-08-06 01:39:36 +0200

    submit/show requests

--- ws/tsws/src/com/tastesync/model/objects/TSUserProfileBasicObj.java
+++ ws/tsws/src/com/tastesync/model/objects/TSUserProfileBasicObj.java
@@ -11,9 +11,14 @@ public class TSUserProfileBasicObj implements Serializable {
     private static final long serialVersionUID = -4536814232029458252L;
     private String userId;
     private String name;
-    private TSPhotoObj photo;
+    private String photo;
+    private TSPhotoObj photoDetails;
 
-    @XmlElement
+    public TSUserProfileBasicObj() {
+		super();
+	}
+
+	@XmlElement
     public String getUserId() {
         return userId;
     }
@@ -24,11 +29,20 @@ public class TSUserProfileBasicObj implements Serializable {
     }
 
     @XmlElement
-    public TSPhotoObj getPhoto() {
+    public String getPhoto() {
         return photo;
     }
+    
+    @XmlElement
+    public TSPhotoObj getPhotoDetails() {
+		return photoDetails;
+	}
 
-    public void setUserId(String userId) {
+	public void setPhotoDetails(TSPhotoObj photoDetails) {
+		this.photoDetails = photoDetails;
+	}
+
+	public void setUserId(String userId) {
         this.userId = userId;
     }
 
@@ -36,7 +50,7 @@ public class TSUserProfileBasicObj implements Serializable {
         this.name = name;
     }
 
-    public void setPhoto(TSPhotoObj photo) {
+    public void setPhoto(String photo) {
         this.photo = photo;
     }
 }
