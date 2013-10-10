88249c65ebeebb09d1bfdd44e556f9f411a32775
dusseltal <jagmohan.khulbe@gmail.com>
2013-10-11 00:28:21 +0200

    change type from class to interface

--- ws/tsws/src/com/tastesync/db/queries/CityQueries.java
+++ ws/tsws/src/com/tastesync/db/queries/CityQueries.java
@@ -1,6 +1,6 @@
 package com.tastesync.db.queries;
 
-public class CityQueries {
+public interface CityQueries {
 	public static String CITY_STATE_SELECT_SQL = "SELECT * FROM cities WHERE state = ? AND city = ?";
 	public static String CITY_SELECT_SQL = "SELECT * FROM cities WHERE city_id = ?";
 	public static String CITY_KEY_STATE_SELECT_SQL = "SELECT * FROM cities WHERE state LIKE ?";
