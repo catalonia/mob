d454f8ef669c55afa9f134588ff3559beecbda4c
Rick Phan <hphu90@gmail.com>
2013-10-14 00:55:04 -0700

    Update CityQueries.java
    Change query getCity

--- ws/tsws/src/com/tastesync/db/queries/CityQueries.java
+++ ws/tsws/src/com/tastesync/db/queries/CityQueries.java
@@ -6,5 +6,5 @@ public interface CityQueries {
 	public static String CITY_KEY_STATE_SELECT_SQL = "SELECT * FROM cities WHERE state LIKE ?";
 	public static String CITY_KEY_CITY_SELECT_SQL = "SELECT * FROM cities WHERE city LIKE ?";
 	//city_neighbourhood
-	public static String CITY_NEIGHBOURHOOD_KEY_DESC_SELECT_SQL = "SELECT * FROM cities, city_neightbourhood WHERE city_neightbourhood.NEIGHBOURHOOD_DESC LIKE ? AND city_neightbourhood.CITYID = cities.city_id";
+	public static String CITY_NEIGHBOURHOOD_KEY_DESC_SELECT_SQL = "SELECT * FROM cities, city_neighbourhood WHERE city_neighbourhood.NEIGHBOURHOOD_DESC LIKE ? AND city_neighbourhood.CITYID = cities.city_id";
 }
